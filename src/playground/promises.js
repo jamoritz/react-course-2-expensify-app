4a789e8cba5f291cd2693eabe2b4951b87051966
Jeff Moritz <moritz@sonic.net>
2018-04-17 01:23:05 -0700

    Finished section 14, lecture 144

---
+++ src/playground/promises.js
@@ -0,0 +1,33 @@
+/*global console */
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve('This is my resolved data');
+        reject('Something went wrong');
+    }, 5000);
+});
+
+console.log('before');
+
+promise
+    .then((data) => {
+        console.log('1', data);
+    })
+    .catch((error) => {
+        console.log('error: ', error);
+    });
+
+promise.then(
+    (data) => {
+        console.log('2', data);
+    },
+    (error) => {
+        console.log('error 2: ', error);
+    }
+);
+
+// promise.then((data) => {
+//     console.log('2', data);
+// });
+
+console.log('after');
