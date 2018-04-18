05eaa5506d8ad385ef08f03f476bafd3ee32a29e
Jeff Moritz <moritz@sonic.net>
2018-04-18 02:50:44 -0700

    Finished section 14, lecture 146

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,41 +15,42 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// database
-//     .ref()
-//     .set({
-//         name: 'Jeff Moritz',
-//         age: 69,
-//         isSingle: false,
-//         location: {
-//             city: 'Rohnert Park',
-//             country: 'United States'
-//         }
-//     })
-//     .then(() => {
-//         console.log('Initial data has been saved.');
-//     })
-//     .catch((error) => {
-//         console.warn('Error setting the initial data.', error);
-//     });
+database
+    .ref()
+    .set({
+        name: 'Jeff Moritz',
+        age: 69,
+        isSingle: false,
+        location: {
+            city: 'Rohnert Park',
+            country: 'United States'
+        }
+    })
+    .then(() => {
+        console.log('Initial data has been saved.');
+    })
+    .catch((error) => {
+        console.warn('Error setting the initial data.', error);
+    });
 
 // database.ref().set('This is my data');
 
-// database
-//     .ref('attributes')
-//     .set({
-//         height: '5-ft - 10-in',
-//         weight: '212.8 lbs'
-//     })
-//     .then(() => {
-//         console.log('Successfully set the height and weight.');
-//     })
-//     .catch((error) => {
-//         console.warn('Error setting the height and weight.', error);
-//     });
+database
+    .ref('attributes')
+    .set({
+        height: '5-ft - 10-in',
+        weight: '212.8 lbs'
+    })
+    .then(() => {
+        console.log('Successfully set the height and weight.');
+    })
+    .catch((error) => {
+        console.warn('Error setting the height and weight.', error);
+    });
 
 // console.log('I made a request to change the data.');
 
+// Preferred way to remove data
 database
     .ref('isSingle')
     .remove()
@@ -59,3 +60,5 @@ database
     .catch((error) => {
         console.warn('Error when attempting to remove the isSingle member.', error);
     });
+
+// database.ref('isSingle').set(null); // This works to remove the object too
