6419705f663c712d6e8262037415b4cc34e19e1f
Jeff Moritz <moritz@sonic.net>
2018-04-23 13:03:28 -0700

    Finished section 15, lecture 155

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: 'AIzaSyCTGK5Xeow7H-lny4G9_bT6u9BM8fPjgMM',
-    authDomain: 'expensify-99f3a.firebaseapp.com',
-    databaseURL: 'https://expensify-99f3a.firebaseio.com',
-    projectId: 'expensify-99f3a',
-    storageBucket: 'expensify-99f3a.appspot.com',
-    messagingSenderId: '1008482088668'
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
