11d01007c8168fcf6863baaf74cd75436aea408a
Jeff Moritz <moritz@sonic.net>
2018-04-25 12:47:35 -0700

    Finished section 16, lecture 162

---
+++ src/actions/auth.js
@@ -0,0 +1,9 @@
+/*global console */
+
+import { firebase, googleAuthProvider } from '../firebase/firebase';
+
+export const startLogin = () => {
+    return () => {
+        return firebase.auth().signInWithPopup(googleAuthProvider);
+    };
+};
